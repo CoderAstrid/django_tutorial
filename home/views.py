903bc0f0723526f1aeb61df2624b0dcbd879a196
CoderAstrid <astrid.wang1229@gmail.com>
2020-05-27 09:58:41 -0700

    added form

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse # added by astrid
+from .models import *
+from .forms import FeedbackForm
 # Create your views here.
 
 #added by astrid
@@ -7,3 +9,13 @@ def index(request):
     msg = 'My Message'
     return render(request, 'home/index.html', {'message': msg})
     # return HttpResponse("Hello, World!")
+
+def create(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/feedback/list')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback.html', {'form': form})
